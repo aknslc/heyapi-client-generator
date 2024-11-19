5c56734c5618e6c7db7a21e424a868dc284b6b18
aknselc <akinsel.coskun@izmirteknoloji.com.tr>
2024-11-19 15:49:00 +0300

    wip

--- examples/next-heyapi-generator/declaration.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'heyapi-client-generator' {
-    export interface ServiceConfig {
-        prefix: string;
-        input: string;
-        output: string;
-        plugins?: string[];
-    }
-    export function generateClients(services: ServiceConfig[]): Promise<void>;
-}
--- examples/next-heyapi-generator/generate-clients.ts
+++ examples/next-heyapi-generator/generate-clients.ts
@@ -3,11 +3,14 @@ import { generateClients, ServiceConfig } from "heyapi-client-generator";
 const services: ServiceConfig[] = [
     {
         prefix: "/api/v1/pre-example",
+        client: "@hey-api/client-axios",
         input: "./openapi.json",
         output: "src/client/pre-example",
+        plugins: ["@tanstack/react-query",],
     },
     {
         prefix: "/api/v1/pre-example2",
+        client: "@hey-api/client-fetch",
         input: "./openapi.json",
         output: "src/client/pre-example2",
     },
